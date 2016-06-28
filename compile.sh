8ce59de20c6a6d8c3f85c2042e59eb74c431f2a4
Kim Kiogora <kimkiogora@gmail.com>
2016-06-28 15:33:16 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #Compile It
 
-gcc -Wall -I/usr/include/mysql -I. -shared lib_mysqludf_sendsms.c -o /usr/lib/lib_mysqludf_sendsms.so
+gcc -Wall -I/usr/include/mysql -I -shared /sms/srcb/lib_mysqludf_sendsms.c -o /usr/lib/lib_mysqludf_sendsms.so
